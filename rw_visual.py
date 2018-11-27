90c5ac875c8ca650c069fe3f0c1a59e967ccc4a3
omzz wezee <omars.e.995@gmail.com>
2018-11-27 11:35:42 -0800

    Plotting the random points with different colors, and gradients, using instances of the random walk class.

--- rw_visual.py
+++ rw_visual.py
@@ -2,10 +2,17 @@ import matplotlib.pyplot as plt
 
 from random_walk import RandomWalk
 
+#Keep porgram running while flag is active
+while True:
+#Create Random walk.
+    rw = RandomWalk()
+    rw.fill_walk()
 
-#Create and plot a Random walk
-rw = RandomWalk()
-rw.fill_walk()
-#Plot
-plt.scatter(rw.x_values, rw.y_values, s=1)
-plt.show()
+    #Plot the walk.
+    point_numbers = list(range(rw.num_points))
+    plt.scatter(rw.x_values, rw.y_values, c = point_numbers, cmap = plt.cm.Blues, edgecolor = 'none', s=10)
+    plt.show()
+
+    keep_running = input("Make another walk?(Y/N)?: ")
+    if keep_running == 'n':
+        break
