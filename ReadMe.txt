0964a68f3b2c38d06296dbad05db8de1e7bd3120
omzz wezee <omars.e.995@gmail.com>
2018-11-27 03:00:02 -0800

    Experimenting with coloring options for data points.

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,3 +1,3 @@
-This is a project I will use to learn more of the data science and visualization aspects of coding.
-This app uses matplotlib to build off of.
+This is a project I will use to learn more of the data science and visualization of data aspects of coding.
+This program uses matplotlib to build off of, and is basically me exploring various types of data representations within the library.
 
--- scatter_squares.py
+++ scatter_squares.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 x_values = list(range(1, 1001))
 y_values = [x ** 2 for x in x_values]
 
-plt.scatter(x_values, y_values, s = 40)
+plt.scatter(x_values, y_values, c= y_values, cmap =  plt.cm.Blues, edgecolor= 'none', s = 40)
 
 #Set chart title and labels
 plt.title('Square Numbers', fontsize = 24)
@@ -15,6 +15,6 @@ plt.ylabel("Square of Value", fontsize = 14)
 plt.tick_params(axis = 'both', which = 'major', labelsize = 14)
 
 #set the range of each axis
-plt.axis
+plt.axis([0, 1100, 0, 1100000])
 
 plt.show()
