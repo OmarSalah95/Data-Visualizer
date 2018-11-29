2a89d7b8487d36c518f48ef2546163e3cfedb263
omzz wezee <omars.e.995@gmail.com>
2018-11-28 19:30:31 -0800

    Few more adjustments. Now highs_lows can parse, convert to F, and store temps.

--- highs_lows.py
+++ highs_lows.py
@@ -1,9 +1,17 @@
 import csv
 
 
+#Stores file location for csv module
 filename = 'imountle13_2018-08-01_2018-09-30.csv'
+
+#Pull and store temps to an array
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
-    print(header_row)
     
+    temps = []
+    for row in reader:
+        #Parse Temperature data and convert to Freedom units and round.
+        temp = round(float(row[2]) * 1.8 + 32)
+        temps.append(temp)
+    print (temps)
