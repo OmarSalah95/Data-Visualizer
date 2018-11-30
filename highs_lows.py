3b4dd6222d4d73c5f21323d3d571e3c2368c7fb4
omzz wezee <omars.e.995@gmail.com>
2018-11-29 17:13:03 -0800

    added error handler, and fresh data set.

--- highs_lows.py
+++ highs_lows.py
@@ -5,7 +5,7 @@ from matplotlib import pyplot as plt
 
 
 #Stores file location for csv module
-filename = 'sitka_weather_2014.csv'
+filename = 'death_valley_2014.csv'
 
 #Pull and store dates, high, and low temps to respective arrays
 with open(filename) as f:
@@ -15,25 +15,26 @@ with open(filename) as f:
     dates, highs, lows = [], [], []
     for row in reader:
         #Strip, format, and store dates.
-        current_date = datetime.strptime(row[0], "%Y-%m-%d")
-        dates.append(current_date)
-        
-        #Strips and stores high temps.
-        high = int(row[1])
-        highs.append(high)
-
-        #Strips and stores lows.
-        low = int(row[3])
-        lows.append(low)
-
+        try:
+            current_date = datetime.strptime(row[0], "%Y-%m-%d")
+            high = int(row[1])
+            low = int(row[3])
+        except ValueError:
+            print(current_date, "Missing Data")
+        else:#Store data
+            dates.append(current_date)
+            highs.append(high)
+            lows.append(low)
+      
     #Pass and plot data
-    fig = plt.figure(dpi = 150, figsize = (10, 6))
+    fig = plt.figure(dpi = 128, figsize = (10, 6))
     plt.plot(dates, highs, c = 'red', alpha  = 0.5)
     plt.plot(dates, lows, c = 'blue', alpha = 0.5)
     plt.fill_between(dates, highs, lows, facecolor = 'blue', alpha = 0.1)
 
     #Format graphic
-    plt.title("Daily high and low temperatures - 2014", fontsize = 24)
+    title = "Daily high and low temperatures - 2014|nDeath Valley, CA"
+    plt.title(title, fontsize = 20)
     fig.autofmt_xdate()
     plt.ylabel("Temperature(F)", fontsize = 16)
     plt.tick_params(axis = 'both', which = 'major',labelsize = 16)
