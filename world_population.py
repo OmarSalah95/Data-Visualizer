a736123a8ba5585b42365f7d1735c81a76d91aa4
omzz wezee <omars.e.995@gmail.com>
2018-11-30 14:50:46 -0800

    Added world map highlighting with population count pop up.

--- world_population.py
+++ world_population.py
@@ -1,5 +1,5 @@
 import json
-
+from pygal.maps.world import World
 from country_codes import get_country_codes
 
 
@@ -8,14 +8,20 @@ from country_codes import get_country_codes
 filename = 'Resources\population_data.json'
 with open(filename) as f:
     pop_data = json.load(f)
+#Build a dictionairy of population data.
+cc_populations = {}
 
-#Print the 2010 population for each country.
 for pop_dict in pop_data:
     if pop_dict['Year'] == '2010':
         country_name = pop_dict['Country Name']
         population = int(float(pop_dict['Value']))
         code = get_country_codes(country_name)
         if code:
-            print(code +": " + str(population))
-        else:
-            print('ERROR - '+ country_name)
+            cc_populations[code] = population
+
+wm = World()
+wm.title = "World Ppopulation in 2010, by Country"
+wm.add('2010', cc_populations)
+
+wm.render_to_file('world_population.svg')
+
