d504ae7892aaed50cf92fc13ad72e152f72667aa
omzz wezee <omars.e.995@gmail.com>
2018-11-29 16:43:30 -0800

    changed the data set used. Parses through the csv file for high temps and draws the to figure,

--- highs_lows.py
+++ highs_lows.py
@@ -1,17 +1,28 @@
 import csv
 
-
+from matplotlib import pyplot as plt
 #Stores file location for csv module
-filename = 'imountle13_2018-08-01_2018-09-30.csv'
+filename = 'sitka_weather_07-2014.csv'
 
-#Pull and store temps to an array
+#Pull and store high temps to an array
 with open(filename) as f:
     reader = csv.reader(f)
     header_row = next(reader)
     
-    temps = []
+    highs = []
     for row in reader:
         #Parse Temperature data and convert to Freedom units and round.
-        temp = round(float(row[2]) * 1.8 + 32)
-        temps.append(temp)
-    print (temps)
+        high = int(row[1])
+        highs.append(high)
+
+    #Pass data to matplot
+    fig = plt.figure(dpi = 150, figsize = (10, 6))
+    plt.plot(highs, c = 'red')
+
+    #Format graphic
+    plt.title("Daily high temperatures, July 2014, fotsize = 24")
+    plt.xlabel('', fontsize = 16)
+    plt.ylabel("Temperature(F)", fontsize = 16)
+    plt.tick_params(axis = 'both', which = 'major',labelsize = 16)
+
+    plt.show()
