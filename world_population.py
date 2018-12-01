3afab5f6f5c829d937a2b2f094066727dec36b1c
omzz wezee <omars.e.995@gmail.com>
2018-11-30 20:04:12 -0800

    Added grouping to world population map.

--- world_population.py
+++ world_population.py
@@ -18,10 +18,26 @@ for pop_dict in pop_data:
         code = get_country_codes(country_name)
         if code:
             cc_populations[code] = population
+#Grup the countries by population
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop <1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+
+#Check country count per group
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3)) 
+
+
 
 wm = World()
-wm.title = "World Ppopulation in 2010, by Country"
-wm.add('2010', cc_populations)
+wm.title = "World Population in 2010, by Country"
+wm.add('0 - 10m', cc_pops_1)
+wm.add('10M-1BN', cc_pops_2)
+wm.add('>1BN', cc_pops_3)
 
 wm.render_to_file('world_population.svg')
 
