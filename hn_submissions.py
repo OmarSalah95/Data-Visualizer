4139d67ecad978adef4f83335e2a2fb67e5bc621
omzz wezee <omars.e.995@gmail.com>
2018-12-02 17:11:07 -0800

    Still can not solve comment counter issue.

--- hn_submissions.py
+++ hn_submissions.py
@@ -21,7 +21,7 @@ for submission_id in submission_ids[:30]:
     submission_dict = {
         'title': response_dict['title'],
         'link' : 'https://news.ycombinator.com/item?id='+ str(submission_id),
-        'comments': response_dict.get('decendents', 0),
+        'comments': response_dict.get('decendents', 0)
         }
     submission_dicts.append(submission_dict)
 
